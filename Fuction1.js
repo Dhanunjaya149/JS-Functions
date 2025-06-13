c8d1ca306125cffd57008e5f39cb598323d0f59e
dhanunjaya5058546@gmail.com <dhanunjaya5058546@gmail.com>
2025-06-13 04:29:14 +0000

    Added

---
+++ Fuction1.js
@@ -0,0 +1,45 @@
+ //Functions are one of the most powerful features in JavaScript.
+ //They allow you to reuse the code, Organize logic, and handle dynamic inputs.
+
+ //Functions are always used like;
+ //declaration+call
+
+ //Syntax
+ //Function Declaration
+//  function sayMyName(){
+//     console.log("Rakesh")
+//  }
+
+//  //Function Calling 
+//  sayMyName()
+//  sayMyName()
+
+let a=10;
+console.log(a)
+
+sayMyName()
+ function sayMyName(){
+    console.log("Rakesh")
+ }
+
+//Functions with parameters
+
+// function sum(){
+//    console.log(10+5)
+// }
+// sum()
+function sum(num1,num2,num3){
+   console.log(num1+num2+num3)
+}
+sum(3,4,-3)
+sum(0,4,0)
+sum(7,0,-3)
+
+function mul(num11,num12,num13){
+   console.log(num11*num12*num13)
+}
+mul(3,4,-3)
+mul(0,4,0)
+mul(7,0,-3)
+
+
